94c4405552b2b50eec4dc0691711d49bbb1de11d
Tadeusz <tadeusz.m.lewandowski@gmail.com>
2016-04-23 23:28:43 +0200

    Add config file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var config = require('./config.js');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -7,7 +8,7 @@ app.get('/', function (req, res) {
    res.render('index.html');
 });
 
-var server = app.listen(7000, function () {
+var server = app.listen(config.port, function () {
   var port = server.address().port;
   console.log("Example app listening at " + port + " port");
 });
