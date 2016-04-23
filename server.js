33e05da315982399de54d6dbc3826f85e6e9a893
Tadeusz <tadeusz.m.lewandowski@gmail.com>
2016-04-23 23:37:57 +0200

    Connect to database

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 var config = require('./config.js');
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname + '/public'));
+
+mongoose.connect(config.dbname, function(err){
+  if(err){
+    console.log(err);
+  } else {
+    console.log("Connected to database");
+  }
+});
+
 
 app.get('/', function (req, res) {
    res.render('index.html');
