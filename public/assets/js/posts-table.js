285465c2a6fc9e6a333283b5f2028f53d8500782
Tadeusz <tadeusz.m.lewandowski@gmail.com>
2016-04-30 20:20:33 +0200

    Add posts header and button to create new post

---
+++ public/assets/js/posts-table.js
@@ -0,0 +1,95 @@
+var PostRow = React.createClass({
+  render: function() {
+    var date = this.props.date.toString().substring(0,10);
+    return (
+      <li>
+        <div className="column-admin">
+          <h4>{this.props.title}</h4>
+        </div>
+        <div className="column-admin">
+          <h4>{this.props.content}</h4>
+        </div>
+        <div className="column-admin">
+          <h4>{this.props.author}</h4>
+        </div>
+        <div className="column-admin">
+          <h4>{date}</h4>
+        </div>
+        <div className="column-admin">
+          <button className="edit-button">Edit</button>
+        </div>
+        <div className="column-admin">
+          <button className="delete-button">Delete</button>
+        </div>
+      </li>
+    );
+  }
+});
+
+var PostsTable = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    var posts = this.state.data.reverse().map(function(post) {
+      var longText = {
+        title : function(){
+          if(post.title.length > 10)
+            return post.title.substring(0,10) + " ...";
+          else
+            return post.title;
+        },
+        content : function(){
+          if(post.content.length > 10)
+            return post.content.substring(0,10) + " ...";
+          else
+            return post.content;
+        }
+      }
+      return (
+        <PostRow author={post.author} key={post._id} title={longText.title()} content={longText.content()} date={post.created_at}/>
+      );
+    });
+
+    return (
+      <ul className="admin-posts">
+        <li>
+          <div className="column-admin">
+            <h3>Title</h3>
+          </div>
+          <div className="column-admin">
+            <h3>Content</h3>
+          </div>
+          <div className="column-admin">
+            <h3>Author</h3>
+          </div>
+          <div className="column-admin">
+            <h3>Date</h3>
+          </div>
+          <div className="column-admin"></div>
+          <div className="column-admin"></div>
+        </li>
+        <hr className="table-separator"/>
+        {posts}
+      </ul>
+    );
+  }
+});
+
+ReactDOM.render(
+  <PostsTable url="/api/posts" />,
+  document.getElementById('posts-table')
+);
